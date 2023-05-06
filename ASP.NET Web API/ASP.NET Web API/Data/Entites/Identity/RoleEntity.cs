8d702e40367710c16713fdaf65f4bf2c0925110c
Timur Ulanovsky <ulanovskytimur@gmail.com>
2023-05-06 11:57:24 +0300

    Added Identity Tables

---
+++ ASP.NET Web API/ASP.NET Web API/Data/Entites/Identity/RoleEntity.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ASP.NET_Web_API.Data.Entites.Identity
+{
+    public class RoleEntity : IdentityRole<int>
+    {
+        public virtual ICollection<UserRoleEntity> UserRoles { get; set; }
+    }
+}
